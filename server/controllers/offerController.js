7b9755ffbefb4e039c48b78d04aee5d480a9b773
Alina <kalina20-04@yandex.ru>
2026-02-07 19:21:51 +0300

    Добавлена первая лабораторная работа. 8 семестр

---
+++ server/controllers/offerController.js
@@ -0,0 +1,19 @@
+import Offer from "../models/offer.js";
+
+
+async function getAllOffers(req, res, next){
+    try{
+        const offers = await Offer.findAll();
+        res.send(offers);
+    } catch(error){
+        console.error('Не удалось получить список предложений', error);
+    }
+}
+
+export {getAllOffers};
+
+/*async function getAllOffers(req, res, next){
+    res.status(200).json({message: 'Работает'});
+}
+
+export {getAllOffers};*/
---
+++ server/index.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import * as dotenv from 'dotenv'
+import sequelize from './config/database.js';
+import cors from 'cors'
+import router from './routes/index.js'
+
+import './models/user.js';
+import './models/offer.js';
+import './models/review.js';
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+const start = async () => {
+    try{
+        await sequelize.authenticate();
+        await sequelize.sync();
+        const app = express();
+app.use(cors())
+app.use(express.json())
+app.use('/', router)
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: 'Работает'})
+})
+        app.listen(PORT, () => console.log(`Сервер запущен на порте ${PORT}`));
+    } catch(e){
+        console.log(e);
+    }
+}
+
+start();
